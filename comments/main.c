b0ddd159906fca9bd347d29a4352110cea0780c9
Luke Anderson <landerson61@gmail.com>
2022-02-14 19:54:48 +0000

    Add comments example

---
+++ comments/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+        Block comment
+        spanning multiple
+        lines.
+    */
+    printf("I have a block comment above me!\n");
+
+    // this is a single line comment. Contents ignored until new line
+    printf("I have a single line comment above me and to the right of me!\n"); // this is also ignored
+    return 0;
+}
